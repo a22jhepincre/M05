6e3e586a870eabab309fa7dde5bded567f4c4d06
premi <premi@LAPTOP-8KLR2EHQ>
2023-01-27 08:30:13 +0100

    PRACTICA UF3 1

---
+++ Practica UF3 - 1/src/practica/uf3/pkg1/PracticaUF31.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.uf3.pkg1;
+
+/**
+ *
+ * @author premi
+ */
+public class PracticaUF31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
