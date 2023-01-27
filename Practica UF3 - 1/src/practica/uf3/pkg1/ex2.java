3c401be150a38360701aa4db74acba81dd4593fb
premi <premi@LAPTOP-8KLR2EHQ>
2023-01-27 09:34:51 +0100

    Practica UF3 1 (ex1 terminado, ex2 en proceso y falta comentar)

--- Practica UF3 - 1/src/practica/uf3/pkg1/ex2.java
+++ Practica UF3 - 1/src/practica/uf3/pkg1/ex2.java
@@ -1,18 +1,27 @@
-package practica.uf3.pkg1;
 
+package practica.uf3.pkg1;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class ex1 {
+/*2. Amplia el programa anterior per tal que:
+a) Si el fitxer no existeix, que el generi.
+b) Si el fitxer ja existeix, que afegeixi el contingut al final.
+c) Si l’usuari escriu @ESBORRA, cal esborrar el contingut del fitxer.
+*/
 
+public class ex2 {
+    
     public static Scanner s = new Scanner(System.in);
-
+    
     public static void main(String[] args) throws IOException {
         File f = new File("C:\\ficheros/textos.txt");
-   
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+
         FileWriter writer = new FileWriter(f, true);
         PrintWriter pw = new PrintWriter(writer);
 
@@ -24,5 +33,5 @@ public class ex1 {
 
         writer.close();
     }
-
+    
 }
