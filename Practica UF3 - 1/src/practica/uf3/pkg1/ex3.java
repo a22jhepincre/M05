f7617660ed43953d20f6891c8d0b25a822443b67
premi <premi@LAPTOP-8KLR2EHQ>
2023-01-31 10:54:15 +0100

    Practica UF3 1 (progreso del ex3, me falta hacer el caso 2 y 3)

--- Practica UF3 - 1/src/practica/uf3/pkg1/ex3.java
+++ Practica UF3 - 1/src/practica/uf3/pkg1/ex3.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import static utils.Utils.*;
 
 public class ex3 {
 
@@ -14,8 +15,11 @@ public class ex3 {
 
     public static void main(String[] args) throws IOException {
         File f = new File("C:\\ficheros/textos.txt");
+        boolean existe = false;
+
         if (!f.exists()) {
             f.createNewFile();
+            existe = true;
         }
 
         FileWriter writer = new FileWriter(f, true);
@@ -35,14 +39,48 @@ public class ex3 {
         pw.flush();
 
         writer.close();
-        
-        FileReader reader = new FileReader(f);
-        BufferedReader buffer = new BufferedReader(reader);
-        
-        String linea = buffer.readLine();
-        while(linea != null){
-            System.out.println(linea);
-            linea = buffer.readLine();
+
+        boolean salir = false;
+        int opcion;
+
+        while (!salir) {
+            System.out.println("1. Leer fichero\n2. Mostrar x linea\n3. Insertar una linea al comencament del fitxer\n");
+            opcion = demanarEnter("Introduce tu opcion: ");
+            switch (opcion) {
+                case 1 -> {
+                    FileReader reader = new FileReader(f);
+                    BufferedReader buffer = new BufferedReader(reader);
+
+                    String linea = buffer.readLine();
+                    while (linea != null) {
+                        System.out.println(linea);
+                        linea = buffer.readLine();
+                    }
+                    buffer.close();
+                    reader.close();
+                }
+                case 2 -> {
+                    int numLinea = demanarEnter("Que linea quieres leer? ");
+                    FileReader reader = new FileReader(f);
+                    BufferedReader buffer = new BufferedReader(reader);
+                    System.out.println("Tiene " + buffer.lines().count() + " lineas");
+                    String[] lineasFichero = new String[10];
+                    String linea = buffer.readLine();
+                    int i = 0;
+                    
+                    while(linea != null){
+                        lineasFichero[i] = linea;
+                        System.out.println(lineasFichero[i]);
+                        i++;
+                        linea = buffer.readLine();
+                    }
+                    buffer.close();
+                    reader.close();
+                    
+                    System.out.println("La linea pone: " + lineasFichero[numLinea]);
+                }
+            }
+
         }
     }
 
